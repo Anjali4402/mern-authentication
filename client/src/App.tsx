c84a0e6a8b9f35d3b4dfddad198b4d50dd895278
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-09-14 20:12:03 +0530

    Setup project with react router dom..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import "./App.css";
 import AuthContext from "./contexts/AuthContext";
+import {  BrowserRouter as Router, Routes , Route } from "react-router-dom";
 
 function App() {
 
   const user = useContext(AuthContext);
+  console.log(user);
 
   return (
-    <>
-      <h1>Mern Authentication</h1>
-      <h2>User is {user?.isAuthenticated ? "" : "not"} authenticated.</h2>
-    </>
+    <Router>
+
+      <Routes>
+
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/about" element={<div>About us page</div>} />
+      </Routes>
+    </Router>
   );
 }
 
