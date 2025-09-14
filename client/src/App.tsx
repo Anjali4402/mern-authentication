682f210858da75ac2beb157fb4f165b50e64b709
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-09-14 20:31:18 +0530

    Setup all component with routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,31 @@
 import { useContext } from "react";
 import "./App.css";
 import AuthContext from "./contexts/AuthContext";
-import {  BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Verifyotp from "./components/Verifyotp";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
   const user = useContext(AuthContext);
   console.log(user);
 
   return (
     <Router>
-
       <Routes>
-
-        <Route path="/" element={<div>Home page</div>} />
-        <Route path="/about" element={<div>About us page</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login/:email" element={<Login />} />
+        <Route path="/otp-verification/:email/:phone" element={<Verifyotp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
+
+      <ToastContainer />
     </Router>
   );
 }
